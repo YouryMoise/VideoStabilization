70256712e0744eea40b291fc529fb51831e6a25d
yourym <yourym@mit.edu>
2026-06-24 23:14:35 -0400

    Fixed issue with order of operations for gradients causing vertical edges to never be detected

--- main.go
+++ main.go
@@ -10,34 +10,29 @@ import (
 	// "image/jpeg" // Registers JPEG decoder
 	"image/png"
 	"gonum.org/v1/gonum/mat"
+	"math/cmplx"
 )
 // type Pixel struct {
 // 	[3]uint8 // RGB
 // }
 
 type Frame struct {
-	pixels []float64
+	pixels []float64 // [r0, g0, b0, a0, r1, ...]
 	width int
 	height int
-	channels uint8
+	channels int
 }
 
-func makeFrame(width int, height int, channels uint8) *Frame {
-	pixels := make([]float64, width*height*int(channels))
+func makeFrame(width int, height int, channels int) *Frame {
+	pixels := make([]float64, width*height*channels)
 	frm := Frame{pixels, width, height, channels}
 	return &frm
 }
 
 func getGrayscaleImg(frame *Frame) image.Image {
-	img := image.NewGray(image.Rect(0, 0, frame.GetWidth(), frame.GetHeight()))
-	
-	// Fast memory copy since image.Gray uses an internal []byte slice
+	img := image.NewGray(image.Rect(0, 0, frame.GetWidth(), frame.GetHeight()))	
 	intPixels := make([]uint8, len(frame.pixels))
 	for i := range intPixels {
-		// if (i-1)%4 == 0 {
-		// 	intPixels[i] = 1
-		// 	continue
-		// }
 		intPixels[i] = uint8(frame.pixels[i]*255)
 	}
 
@@ -66,26 +61,26 @@ func (frm *Frame) GetWidth() int {
 	return frm.width
 }
 
-func (frm *Frame) GetChannels() uint8 {
+func (frm *Frame) GetChannels() int {
 	return frm.channels
 }
 
 func (frm *Frame) GetPixelAt(x int, y int) []float64{
 	index := y * frm.GetWidth() + x
 	channels := frm.GetChannels()
-	return frm.pixels[index*int(channels):index*int(channels)+int(channels)]
+	return frm.pixels[index*channels:index*channels+channels]
 }
 
 func (frm *Frame) SetPixelAt(x int, y int, value []float64){
 	index := y * frm.GetWidth() + x
 	channels := frm.GetChannels()
-	copy(frm.pixels[index*int(channels):index*int(channels)+int(channels)], value) 
+	copy(frm.pixels[index*channels:index*channels+channels], value) 
 }
 
 func (frm *Frame) Grayscale() *Frame {
 	height := frm.GetHeight()
 	width := frm.GetWidth()
-	var channels uint8 = 1
+	channels := 1
 	grayfrm := makeFrame(width, height, channels)
 
 	for x := 0; x < width; x++ {
@@ -109,7 +104,7 @@ func (frm *Frame) Gradient(vertical bool) *Frame {
 	height := frm.GetHeight()
 	channels := frm.GetChannels()
 	gradFrm := makeFrame(width, height, channels)
-	for c := uint8(0); c < channels; c++ {
+	for c := 0; c < channels; c++ {
 		if c == 3 {
 			break
 		}
@@ -123,11 +118,9 @@ func (frm *Frame) Gradient(vertical bool) *Frame {
 						-1*frm.GetPixelAt(x-1, y+1)[c] +
 						-2*frm.GetPixelAt(x, y+1)[c] +
 						-1*frm.GetPixelAt(x+1, y+1)[c])
-					// difference := []float64{top[0]-bottom[0], top[1]-bottom[1], top[2]-bottom[2], bottom[3]} // we don't want to change opacity
 					currentPixel := gradFrm.GetPixelAt(x,y)
 					currentPixel[c] = Gy
 					gradFrm.SetPixelAt(x,y,currentPixel) // should be unnecessary
-					// gradFrm.SetPixelAt(x,y, difference)
 				} else {
 					Gx := (
 						-1*frm.GetPixelAt(x-1, y-1)[c] +
@@ -155,8 +148,20 @@ func shiTomasi(frm *Frame, windowWidth int, windowHeight int) *Frame {
 	frameHeight := grayFrm.GetHeight()
 
 	// find Ix Frame
-	Ix := frm.Gradient(false)
-	Iy := frm.Gradient(true)
+	Ix := grayFrm.Gradient(false)
+	Iy := grayFrm.Gradient(true)
+	IxIy := makeFrame(frameWidth, frameHeight, 1)
+	Ix2 := makeFrame(frameWidth, frameHeight, 1)
+	Iy2 := makeFrame(frameWidth, frameHeight, 1)
+	for x := 0; x < frameWidth; x++ {
+		for y := 0; y < frameHeight; y++ {
+			IxValue := Ix.GetPixelAt(x,y)[0]
+			IyValue := Iy.GetPixelAt(x,y)[0]
+			Ix2.SetPixelAt(x,y, []float64{IxValue*IxValue})
+			Iy2.SetPixelAt(x,y, []float64{IyValue*IyValue})
+			IxIy.SetPixelAt(x,y, []float64{IxValue*IyValue})
+		}
+	}
 
 	// use grayscale frame to track R values
 	rFrame := makeFrame(frameWidth, frameHeight, 1)
@@ -167,15 +172,17 @@ func shiTomasi(frm *Frame, windowWidth int, windowHeight int) *Frame {
 		for wy := 0; wy < frameHeight/windowHeight; wy++ {
 			startX := wx*windowWidth
 			startY := wy*windowHeight
-			IxSum := 0.0
-			IySum := 0.0
+			Ix2Sum := 0.0
+			Iy2Sum := 0.0
+			IxIySum := 0.0
 			for x := startX; x < startX + windowWidth; x++ {
 				for y := startY; y < startY + windowHeight; y++ {
-					IxSum += Ix.GetPixelAt(x, y)[0]
-					IySum += Iy.GetPixelAt(x, y)[0]
+					Ix2Sum += Ix2.GetPixelAt(x, y)[0]
+					Iy2Sum += Iy2.GetPixelAt(x, y)[0]
+					IxIySum += IxIy.GetPixelAt(x, y)[0]
 				}
 			}
-			raw_matrix := []float64{IxSum*IxSum, IxSum*IySum, IxSum*IySum, IySum*IySum}
+			raw_matrix := []float64{Ix2Sum, IxIySum, IxIySum, Iy2Sum}
 			matrix := mat.NewDense(2, 2, raw_matrix)
 
 			var eig mat.Eigen
@@ -184,11 +191,9 @@ func shiTomasi(frm *Frame, windowWidth int, windowHeight int) *Frame {
 			}
 			eigenvalues := eig.Values(nil)
 			// e1 is x changes (vertical edges), e0 is y changes (horizontal edges)
-			e0 := real(eigenvalues[0])
-			e1 := real(eigenvalues[1])
-			// currentR := []float64{e0}
+			e0 := cmplx.Abs(eigenvalues[0])
+			e1 := cmplx.Abs(eigenvalues[1])
 			currentR := []float64{min(e0, e1)}
-			// currentR := []float64{min(e0, e1)}
 			maxR = max(maxR, currentR[0])
 
 			for x := startX; x < startX + windowWidth; x++ {
@@ -196,11 +201,10 @@ func shiTomasi(frm *Frame, windowWidth int, windowHeight int) *Frame {
 					rFrame.SetPixelAt(x,y, currentR)
 				}
 			}
-			// fmt.Printf("eigen: %v\n", eigenvalues)
 		}
 	}
 
-	thresholdR := float64(maxR * 0.1)
+	thresholdR := float64(maxR * 0.01)
 	fmt.Printf("threshold %v\n", thresholdR)
 	
 	outputFrm := makeFrame(frameWidth, frameHeight, 1)
@@ -214,22 +218,12 @@ func shiTomasi(frm *Frame, windowWidth int, windowHeight int) *Frame {
 	}
 
 	return outputFrm
-
-
-
-
-	// find Iy Frame
-
-	// for each window (parameterized size), sum Ix^2, Iy^2, IxIy for 2x2 matrix
-
-	// get Eigenvalues of the matrix and say R = min eigenvalue
-
-	// mark all pixels as corners if R is high enough
 }
 
 
 func main() {
 	// Open the video file
+	// video, err := vidio.NewVideo("media/paper2.mp4")
 	video, err := vidio.NewVideo("media/kitchen.mp4")
 	if err != nil {
 		log.Fatalf("Failed to open video: %v", err)
@@ -238,7 +232,8 @@ func main() {
 	fmt.Printf("Hi\n %v %v\n", video.Width(), video.Height())
 	frame_width := video.Width()
 	frame_height := video.Height()
-	var frame_channels uint8 = 4
+	fmt.Printf("%v %v\n", frame_width, frame_height)
+	frame_channels := 4
 
 	// Loop through every frame in the video
 	for video.Read() {
@@ -248,31 +243,16 @@ func main() {
 		// [R0, G0, B0, A0, R1, ...]
 		rawFrameFloats := make([]float64, len(rawFrame))
 		for i := range rawFrameFloats {
-			// if (i-1)%4 == 0 {
-			// 	rawFrameFloats[i] = 1.0
-			// 	continue
-			// }
 			rawFrameFloats[i] = float64(rawFrame[i]) / 255.0
 		}
 		frame := makeFrame(frame_width, frame_height, frame_channels)
-		// grayFrm := frm.Grayscale()
-		
-
 		frame.FillFrame(rawFrameFloats)
 
 		withCorners := shiTomasi(frame, 5, 5)
-
 		// grayFrame := frame.Grayscale()
 		grayImg := getGrayscaleImg(withCorners)
+		// saveToPNG(grayImg, "./second.png")
 		saveToPNG(grayImg, "./first.png")
 		break
-
-
-		// Process your frame here...
-		// fmt.Printf("Frame read. Size: %d bytes\n", len(frame))
-		// fmt.Printf("0 %v 1 %v 2 %v\n", frame[0], frame[1], frame[2])
-		// // fmt.Printf("%T\n", frame)
-		// break
-
 	}
 }
